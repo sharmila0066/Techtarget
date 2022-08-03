b0c1de02b4c98d5013511e46a65ec948f548d3dc
sharmila0066 <110526974+sharmila0066@users.noreply.github.com>
2022-08-03 01:39:40 -0700

    Add files via upload

---
+++ sha1.pyt.py
@@ -0,0 +1,18 @@
+import time
+start=time.time()
+print("List")
+List=[1,2,"ABC",3,"XYZ",2.3]
+print(List)
+print("\nDictionary")
+Dict={"a":1,"b":2,"c":3}
+print(Dict)
+print("\nTuples")
+Tup=(1,2,3,4,5)
+print(Tup)
+print("\nSets")
+s={1,1,2,2,3,3,4,4,5,5}
+print(s)
+end=time.time()
+
+
+
